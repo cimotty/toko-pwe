f2db2fff1d3e19b96cc08544978f98bec7cfc894
cimotty <rizky00164@gmail.com>
2022-04-22 03:20:52 +0700

    memperbarui url

--- routes/web.php
+++ routes/web.php
@@ -28,47 +28,47 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tanpacontroller/1', function () {
+Route::get('/tanpa-controller/1', function () {
     echo '1';
 });
-Route::get('/tanpacontroller/2', function () {
+Route::get('/tanpa-controller/2', function () {
     echo '2';
 });
-Route::get('/tanpacontroller/3', function () {
+Route::get('/tanpa-controller/3', function () {
     echo '3';
 });
-Route::get('/tanpacontroller/4', function () {
+Route::get('/tanpa-controller/4', function () {
     echo '4';
 });
-Route::get('/tanpacontroller/5', function () {
+Route::get('/tanpa-controller/5', function () {
     echo '5';
 });
-Route::get('/tanpacontroller/6', function () {
+Route::get('/tanpa-controller/6', function () {
     echo '6';
 });
-Route::get('/tanpacontroller/7', function () {
+Route::get('/tanpa-controller/7', function () {
     echo '7';
 });
-Route::get('/tanpacontroller/8', function () {
+Route::get('/tanpa-controller/8', function () {
     echo '8';
 });
-Route::get('/tanpacontroller/9', function () {
+Route::get('/tanpa-controller/9', function () {
     echo '9';
 });
-Route::get('/tanpacontroller/10', function () {
+Route::get('/tanpa-controller/10', function () {
     echo '10';
 });
 
-Route::get('/dengancontroller/1', [DenganController1::class, 'index']);
-Route::get('/dengancontroller/2', [DenganController2::class, 'index']);
-Route::get('/dengancontroller/3', [DenganController3::class, 'index']);
-Route::get('/dengancontroller/4', [DenganController4::class, 'index']);
-Route::get('/dengancontroller/5', [DenganController5::class, 'index']);
-Route::get('/dengancontroller/6', [DenganController6::class, 'index']);
-Route::get('/dengancontroller/7', [DenganController7::class, 'index']);
-Route::get('/dengancontroller/8', [DenganController8::class, 'index']);
-Route::get('/dengancontroller/9', [DenganController9::class, 'index']);
-Route::get('/dengancontroller/10', [DenganController10::class, 'index']);
+Route::get('/dengan-controller/1', [DenganController1::class, 'index']);
+Route::get('/dengan-controller/2', [DenganController2::class, 'index']);
+Route::get('/dengan-controller/3', [DenganController3::class, 'index']);
+Route::get('/dengan-controller/4', [DenganController4::class, 'index']);
+Route::get('/dengan-controller/5', [DenganController5::class, 'index']);
+Route::get('/dengan-controller/6', [DenganController6::class, 'index']);
+Route::get('/dengan-controller/7', [DenganController7::class, 'index']);
+Route::get('/dengan-controller/8', [DenganController8::class, 'index']);
+Route::get('/dengan-controller/9', [DenganController9::class, 'index']);
+Route::get('/dengan-controller/10', [DenganController10::class, 'index']);
 
 Route::get('/profil', [ProfilController::class, 'index']);
 
